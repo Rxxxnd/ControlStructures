109b95524d158e572cc12a7db57fb0f869e68e1d
tehiv <tehiv@ASUS-ROG-DSKTP>
2023-03-01 17:32:52 +0300

    Pascal triangle done.

--- Geometry/main.cpp
+++ Geometry/main.cpp
@@ -10,15 +10,15 @@
 //#define RHOMBUS
 //#define PLUS_MINUS
 //#define PLUS_MINUS_V2
-#define PASCAL_PYRAMID
+#define PASCAL_TRIANGLE
 
 //#define CHESS_DESK
 void main()
-{
+{  /*
 	setlocale(LC_ALL, "");
 	int n;
 	std::cout << "Enter the size of figure: "; std::cin >> n;
-
+	*/
 #ifdef SQUARE
 	for (int i = 0; i < n; i++)
 	{
@@ -151,9 +151,37 @@ void main()
 
 #endif // PLUSMINUS
 
-#ifdef PASCAL_PYRAMID
+#ifdef PASCAL_TRIANGLE
+	
+	int h;
+	std::cout << "Enter the triangle height: "; std::cin >> h;
 
+	long long int nf = 1;
+	for (int i = 0; i <= h; i++)std::cout << "  ";
+	std::cout << "   ";
+	std::cout << 1 << std::endl;
+	for (int n = 1; n <= h; n++)
+	{
+		for (int i = n % 2 == 0 ? 1 : 0; i <= h - n; i++)std::cout << "  ";
+		if (n % 2 == 0)std::cout << "  ";
+		nf *= n;
+		long long int kf = 1;
+		std::cout.width(4);
+		std::cout << 1;// << "\t";
+		for (int k = 1; k <= n; k++)
+		{
+			kf *= k;
+			long long int nkf = 1;
+			for (int nk = 1; nk <= n - k; nk++)
+			{
+				nkf *= nk;
+			}
+			std::cout.width(4);
+			std::cout << nf / kf / nkf;// << "\t";
+		}
+		std::cout << std::endl;
+	}
 
-#endif // CHESS
-
+#endif PASCAL_TRIANGLE //Pascal triangle3
 }
+
